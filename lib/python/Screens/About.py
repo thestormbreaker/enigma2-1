6fa92db1019cac4170ad964f2a52fce39641c60d
meo <lupomeo@hotmail.com>
2016-02-05 17:13:08 +0100

    fix image informations

--- lib/python/Screens/About.py
+++ lib/python/Screens/About.py
@@ -114,10 +114,10 @@ class About(Screen):
 			})
 
 	def populate(self):
-		self["lab1"] = StaticText(_("openATV"))
-		self["lab2"] = StaticText(_("By openATV Image Team"))
+		self["lab1"] = StaticText(_("openBH"))
+		self["lab2"] = StaticText(_("By BlackHole Team"))
 		model = None
-		self["lab3"] = StaticText(_("Support at") + " www.opena.tv")
+		self["lab3"] = StaticText(_("Support at") + " www.vuplus-community.net")
 
 		AboutText = getAboutText()[0]
 
@@ -262,8 +262,8 @@ class SystemMemoryInfo(Screen):
 		Screen.__init__(self, session)
 		Screen.setTitle(self, _("Memory Information"))
 		self.skinName = ["SystemMemoryInfo", "About"]
-		self["lab1"] = StaticText(_("openATV"))
-		self["lab2"] = StaticText(_("By openATV Image Team"))
+		self["lab1"] = StaticText(_("openBH"))
+		self["lab2"] = StaticText(_("By BlackHole Team"))
 		self["AboutScrollLabel"] = ScrollLabel()
 
 		self["actions"] = ActionMap(["SetupActions", "ColorActions"],
@@ -530,7 +530,7 @@ class SystemNetworkInfo(Screen):
 class AboutSummary(Screen):
 	def __init__(self, session, parent):
 		Screen.__init__(self, session, parent=parent)
-		self["selected"] = StaticText("openATV:" + getImageVersion())
+		self["selected"] = StaticText("openBH:" + getImageVersion())
 
 		AboutText = getAboutText()[1]
 
@@ -582,7 +582,7 @@ class ViewGitLog(Screen):
 		fd = open('/etc/' + self.logtype + '-git.log', 'r')
 		releasenotes = fd.read()
 		fd.close()
-		releasenotes = releasenotes.replace('\nopenatv: build', "\n\nopenatv: build")
+		releasenotes = releasenotes.replace('\nopenbh: build', "\n\nopenbh: build")
 		self["text"].setText(releasenotes)
 		summarytext = releasenotes
 		try:
